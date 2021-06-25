373a3867f3184f7c3f722cc33f32c1cd4a277c47
Alex <vasilax@gmail.com>
2021-06-25 12:43:59 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,15 +19,3 @@ resource "aws_dynamodb_table" "tfc_example_table" {
   }
 }
 
-resource "aws_dynamodb_table" "tfc_example_table-2" {
-  name = "${var.db_table_name}-${random_pet.table_name.id}-1"
-
-  read_capacity  = var.db_read_capacity
-  write_capacity = var.db_write_capacity
-  hash_key       = "UUID"
-
-  attribute {
-    name = "UUID"
-    type = "S"
-  }
-}
